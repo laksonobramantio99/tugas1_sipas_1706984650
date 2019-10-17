81c34f5faf87728a18157e6868c43426843e7d02
Laksono Bramantio <laxbramantio@gmail.com>
2019-10-17 23:39:40 +0700

    feat: Finish 'View All Diagnosis Penyakit' feature

---
+++ src/main/java/apap/tugas1/sipas/service/DiagnosisPenyakitService.java
@@ -0,0 +1,9 @@
+package apap.tugas1.sipas.service;
+
+import apap.tugas1.sipas.model.DiagnosisPenyakitModel;
+
+import java.util.List;
+
+public interface DiagnosisPenyakitService {
+    List<DiagnosisPenyakitModel> getAllDiagnosisPenyakit();
+}
